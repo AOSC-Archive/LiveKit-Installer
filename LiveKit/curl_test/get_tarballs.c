4330d2653fca1f125587f3c4fdc32aaa80f57806
Trickness <sternwzhang@outlook.com>
2015-02-23 07:42:30 -0500

    progress show

--- LiveKit/curl_test/get_tarballs.c
+++ LiveKit/curl_test/get_tarballs.c
@@ -11,6 +11,11 @@ size_t write_data(void *buffer, size_t size, size_t nmemb, void *user_p){
     return return_size;
 }
 
+size_t progress_func(char *progress_data, double dltotal, double dlnow,double ultotal, double ulnow){
+    printf("%s %g / %g (%g %%)\n", progress_data, dlnow, dltotal, dlnow*100.0/dltotal);
+    return 0;
+}
+
 int main(){
     CURLcode return_code;
     return_code = curl_global_init(CURL_GLOBAL_ALL);        // initialize everything possible
@@ -23,8 +28,12 @@ int main(){
         fprintf(stderr,"get a easy handle failed!\n");
         curl_global_cleanup();
     }
+    char *progress_data = "* ";
     FILE *fp = fopen("OS3Beta.tar.xz","ab+");
     curl_easy_setopt(easy_handle,CURLOPT_URL,"http://mirrors.ustc.edu.cn/anthon/os3-releases/01_Beta/01_Tarballs/aosc-os3_mate-beta_pichu_dpkg_en-US.tar.xz");
+    curl_easy_setopt(easy_handle, CURLOPT_NOPROGRESS, 0);
+    curl_easy_setopt(easy_handle, CURLOPT_PROGRESSDATA,progress_data);
+    curl_easy_setopt(easy_handle, CURLOPT_PROGRESSFUNCTION, progress_func);
     curl_easy_setopt(easy_handle, CURLOPT_WRITEFUNCTION, &write_data);
     curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, fp);
 
