c62147c3f393b499166f0bf7aa455796c4bc5520
Mingye Wang <arthur200126@gmail.com>
2015-10-20 09:20:44 -0400

    不要乱缩进…修不动伤不起

--- BashDialog/inst.sh
+++ BashDialog/inst.sh
@@ -6,10 +6,10 @@ ended () {
 }
 
 welcome () {
-	  dialog --msgbox	"`cat welcome.${LANG}`"	6 30
-	! dialog --yesno	"`cat license.${LANG}`"	10 40 && ended
-	# dialog --textbox	"LGPL.${LANG}"		10 40
-	! dialog --yesno	"`cat start.${LANG}`"	6 30 && ended
+	dialog --msgbox	"`cat welcome.${LANG}`"	6 30
+	dialog --yesno	"`cat license.${LANG}`"	10 40 || ended
+#	dialog --textbox "LGPL.${LANG}"		10 40
+	dialog --yesno "`cat start.${LANG}`" 6 30 || ended
 }
 
 partition () {
